3acba1fe82e326221d49612a031132cc6e915365
calves <carlos.j2sdk@gmail.com>
2023-04-20 10:07:42 -0300

    adicionado documentacao swagger

--- app/controller/TarefaController.py
+++ app/controller/TarefaController.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, json
+from flask_openapi3 import OpenAPI, Info, Tag
 
 from app.model.Tarefa import Tarefa
 from app.response.TarefaResponse import TarefaResponse
@@ -6,33 +7,42 @@ from app.service.TarefaService import TarefaService
 
 app = Flask(__name__)
 
+info = Info(title="Listas de tarefas API", version="1.0.0")
+app = OpenAPI(__name__, info=info)
 
-@app.route('/tarefa/nova', methods=['POST'])
+tarefas_tag = Tag(name="tarefa", description="Tarefas")
+
+
+@app.post('/tarefa/nova', summary="Criar nova tarefa", tags=[tarefas_tag])
 def adicionar_tarefa():
     tarefa_request = request.json
     tarefa = Tarefa(1, tarefa_request['titulo'], tarefa_request['descricao'], 'false', tarefa_request['data'])
     tarefa_gravada = TarefaService().gravar(tarefa)
     return jsonify(tarefa_gravada.__dict__)
 
-@app.route('/tarefa/todas', methods=['GET'])
+
+@app.get('/tarefa/todas', summary="Listar todas as tarefas", tags=[tarefas_tag])
 def listar_tarefas():
     tarefas = TarefaService().listar()
     schema = TarefaResponse(many=True)
     return jsonify(schema.dump(tarefas))
 
-@app.route('/tarefa/<int:id>', methods=['GET'])
+
+@app.get('/tarefa/<int:id>', summary="Recuperar tarefa por id tarefa", tags=[tarefas_tag])
 def recuperar_tarefa_por_id(id):
     tarefa = Tarefa(id, '', '', '', '')
     tarefa_recuperada = TarefaService().buscar_por_id(tarefa)
     return  json.dumps(tarefa_recuperada.__dict__), 200, {'ContentType': 'application/json'}
 
-@app.route('/tarefa/finalizada/<int:id>', methods=['PUT'])
+
+@app.put('/tarefa/finalizada/<int:id>', summary="Concluir uma tarefa por id", tags=[tarefas_tag])
 def finalizar_tarefa_por_id(id):
     tarefa = Tarefa(id, '', '', '', '')
     TarefaService().finalizar(tarefa)
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
-@app.route('/tarefa/removida/<int:id>', methods=['DELETE'])
+
+@app.delete('/tarefa/removida/<int:id>', summary="Apagar tarefa por id", tags=[tarefas_tag])
 def apagar_tarefa(id):
     tarefa = Tarefa(id, '', '', '', '')
     TarefaService().excluir(tarefa)
